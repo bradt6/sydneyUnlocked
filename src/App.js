31f48ce944d7db52ba4a053702d2a85263e1549d
Brad T <bradthurlow@gmail.com>
2020-02-07 18:03:51 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Card from './Ui/Card'
+
 
 function App() {
+  const [getEventState, setEventState] = useState({
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1> hi this is my ract project </h1>
+      </div>
   );
 }
 
---
+++ src/Ui/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Card.css';
+
+const card = (props) => {
+    return (
+        <div className="Card">
+
+        </div>
+    )
+};
+
+export default card;
