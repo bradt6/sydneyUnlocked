a70a7644838b18e35fe4ff2c9638393117a1fffb
Brad T <bradthurlow@gmail.com>
2020-02-20 17:47:54 +1100

    added map

--- src/components/dateNavigation/DateNav.js
+++ src/components/dateNavigation/DateNav.js
@@ -71,15 +71,55 @@ const ScrollableTabsButtonAuto = (props) => {
 
     const handleChange = (event, newValue) => {
         console.log("HANDLE CHANGE: ", newValue)
+        console.log("EVEnt: ", event)
         setValue(newValue);
     };
     console.log(isoArr);
+
+    const appendToEvents = (values, dateKey) => {
+        // const current= [...getEvents];
+        let objectsDic = new Map();
+        // THIS NEEDS TO SORTED BY detailedStart
+        var data = JSON.parse(values);
+
+        var dataObjects = data.map(event => ({
+            id: event.EventID,
+            eventName: event.event_name[0],
+            venueName: event.venue_name,
+            date: event.Full_date,
+            startTime: event.start_time,
+            endTime: event.end_time,
+            ticketLink: event.ticketLink,
+            detailed_start: event.detailedStart
+        }));
+        objectsDic.set(dateKey, dataObjects);
+        // console.log("APPEND TO EVENTS VALUES FULL: ",typeof values);
+        // console.log("THIS IS JSON PARSE: ", data);
+        console.log(dataObjects);
+        console.log(objectsDic);
+        // setEvents(value);
+
+    }
+
+    // current_event = [
+    //     {
+    //         id: 'YUIDYSAJHJDK21343124',
+    //         eventName: 'Australia party',
+    //         venueName: 'Ivy',
+    //         date: '10/02.20',
+    //         startTime: '2000',
+    //         endTime: '0100',
+    //         ticketLink: 'www.example.com'
+    //     }
 
     React.useEffect(() => {
         console.log("IN THE USE EFFECT: ", isoArr[value]);
         axios.get('https://gc5o17bgv6.execute-api.ap-southeast-2.amazonaws.com/dev/get-events/all/' + '?ISO_Date=' + isoArr[value])
             .then(response => {
                 console.log(response);
+                // console.log(response.data.body);
+                appendToEvents(response.data.body, isoArr[value])
+                // appendToEvents(response, isoArr[value])
             });
     });
 
